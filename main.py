7db055eca91e40e57f5360070b5486ea38576af5
Mahmoud Hashem <89657916+mm-hashem@users.noreply.github.com>
2026-05-21 00:05:10 +0300

    refactor(main): improve logging setup and initialize GameOfLife with parameters

--- main.py
+++ main.py
@@ -1,22 +1,33 @@
+import sys
 import logging
 import tkinter as tk
 from engine import GameOfLife
 from rle_manager import RLEManager
 from gui import GameOfLifeGUI
 
+# Check if running in Pyodide
+IS_PYD = (sys.platform == "emscripten")
+
 def main() -> None:
-    logging.basicConfig(
-        filename='gol.log', filemode='w', level=logging.INFO,
-        format='%(asctime)s - %(levelname)s - %(module)s - %(funcName)s - %(message)s'
-    )
+    if IS_PYD:
+        logging.disable(logging.CRITICAL)
+    elif __debug__:
+        logging.basicConfig(
+            filename='gol.log', filemode='w', level=logging.INFO,
+            format='%(asctime)s - %(levelname)s - %(filename)s - %(module)s - %(funcName)s - %(lineno)d - %(message)s'
+        )
 
     root = tk.Tk()
     logging.info("Instantiated Tkinter")
 
-    engine = GameOfLife()
+    ROWS = 50
+    COLS = 50
+    PATTERNS_FOLDER = "patterns"
+
+    engine = GameOfLife(ROWS, COLS)
     logging.info("Instantiated GameOfLife")
 
-    rle_manager = RLEManager()
+    rle_manager = RLEManager(PATTERNS_FOLDER)
 
     gui = GameOfLifeGUI(root, engine, rle_manager)
     logging.info("Instantiated GameOfLifeGUI")
